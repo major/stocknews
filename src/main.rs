228a756bfa1dc17faac7c1d6b6fe718a8ce909dc
Major Hayden <major@mhtx.net>
2026-07-07 16:56:03 -0500

    feat: wire up Sentry error reporting (#537)
    - add sentry crate (rustls-only transport, no native-tls/openssl)
    
    - init Sentry conditionally when SENTRY_DSN is set, before tracing setup
    
    - wire sentry-tracing layer into the tracing_subscriber registry
    
    - document SENTRY_DSN/SENTRY_DEBUG and other env vars in README
    
    Signed-off-by: Major Hayden <major@mhtx.net>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use stocknews::{alpaca::run, config::Settings};
+use tracing_subscriber::prelude::*;
 
 #[tokio::main]
 async fn main() -> stocknews::alpaca::AppResult<()> {
-    tracing_subscriber::fmt::init();
     let settings = Settings::from_env()?;
+
+    let _sentry_guard = (!settings.sentry_dsn.is_empty()).then(|| {
+        sentry::init(sentry::ClientOptions {
+            dsn: settings.sentry_dsn.parse().ok(),
+            debug: settings.sentry_debug,
+            release: sentry::release_name!(),
+            traces_sample_rate: 0.0,
+            ..Default::default()
+        })
+    });
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(sentry::integrations::tracing::layer())
+        .init();
 
     tokio::select! {
         () = run(&settings) => Ok(()),
